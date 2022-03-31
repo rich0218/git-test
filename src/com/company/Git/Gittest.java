be40bb540eb6814e03e278c3e4db1b5816f56357
rich0218 <491831016@qq.com>
2022-03-31 15:16:46 +0800

    测试git

---
+++ src/com/company/Git/Gittest.java
@@ -0,0 +1,12 @@
+package com.company.Git;
+
+/**
+ * @author xia
+ * @date 2022/3/31
+ * @desc
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hellox");
+    }
+}
